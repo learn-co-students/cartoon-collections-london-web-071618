8d03b641456fed72060cc91f2a000400cdb212b9
Adham Muhammad <admuaq@gmail.com>
2018-07-10 13:49:54 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(order)# code an argument here
   # Your code here
+  order.each_with_index do |element, index|
+   puts "#{index+1}. #{element}"
+  end 
+  
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(call)# code an argument here
   # Your code here
+  call.collect do |element|
+    element.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(word)# code an argument here
   # Your code here
+  word.any? do |element|
+    element.length > 4
+  end 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(squeek)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  squeek.find do |element|
+    element == "cheddar"
+  end
+  
 end
